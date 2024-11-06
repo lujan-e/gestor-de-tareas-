ec58c62414455307498fe3427d92b7e1c3e9d4f8
lujan <espinozamarialujan94@gmail.com>
2024-11-06 17:40:34 -0300

    nuevo

--- gestor-tarea/vue.config.js
+++ gestor-tarea/vue.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/gestor-tarea/'
+    ? '/gestor-de-tarea/' // Cambia esto al nombre correcto de tu repositorio
     : '/'
 });
+
